b1c44a063e55ba1260c8bdf2ff91d16c552d136e
Victor Rodrigues <victor.rodrigues@itbeta2.com.br>
2026-01-27 23:42:18 -0300

    first commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import './styles.css';
+
+export default function Header() {
+
+    return (
+        <header className="car-header">
+            <nav className="car-container">
+                <h1>Carros Top</h1>
+            </nav>
+        </header>
+    );
+}

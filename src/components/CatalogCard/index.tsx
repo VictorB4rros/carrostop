c57cc991af29e0ce74a65f1510130268827cc85f
Victor Rodrigues <victor.rodrigues@itbeta2.com.br>
2026-01-29 16:04:41 -0300

    car card adjustments

--- src/components/CatalogCard/index.tsx
+++ src/components/CatalogCard/index.tsx
@@ -5,17 +5,19 @@ import CardButton from '../CardButton';
 export default function CatalogCard() {
     return (
         <div className="car-card">
-            <div className="car-card-image">
+            <div className="car-card-image car-mt10">
                 <img src={carImg} alt="Car" />
             </div>
             <div className="car-card-text">
-                <h2>Audi Supra TT</h2>
-                <h4>
-                    Lorem ipsum dolor sit amet, consectetur 
+                <h2 className="car-mb15">Audi Supra TT</h2>
+                <h4 className="car-mb20 car-mt15">
+                    Lorem ipsum dolor sit amet, consectetur
                     adipisicing elit. Cupiditate, nisi
                 </h4>
             </div>
-            <CardButton />
+            <div className="car-mt20">
+                <CardButton />
+            </div>
         </div>
     );
 }
